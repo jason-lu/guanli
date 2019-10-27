8cf53ec6b6e74593a5ceb50ca783edf2a30516bc
wamgyx <wamgyx>
2019-10-27 15:35:23 +0800

    增加场馆

--- src/pages/ActivityContainer.vue
+++ src/pages/ActivityContainer.vue
@@ -1,84 +1,124 @@
 <template>
-    <div class="activity-container">
-        <div class="box" v-for="(item, index) in activityList"  :key="index">
-            <img class="preview" src="../assets/img/activities-preview.jpg" />
-            <div class="content">
-                {{item.title}} <br />
-                {{item.type}}
-                <router-link id="detail" to="/activityListDetail">详情</router-link>
-                <!-- <button id="detail">详情</button> -->
+    <div class="actContainer">
+        <!-- 热点活动 -->
+        <div class="hotActivity">
+            热点活动
+        </div>
+        <div class="activity-container">
+            <div class="box" v-for="(item, index) in hotActivityList" :key="index">
+                <img class="preview" src="../assets/img/activities-preview.jpg" />
+                <div class="content">
+                    {{item.title}} <br />
+                    {{item.type}} <br /> <br />
+                    热度：<el-rate v-model="value5" disabled show-score text-color="#ff9900" score-template="{value}">
+                    </el-rate>
+                    <router-link id="detail" to="/activityListDetail">详情</router-link>
+                    <!-- <button id="detail">详情</button> -->
+                </div>
+            </div>
+        </div>
+        <!-- 全部活动 -->
+        <div class="allActivity">
+            全部活动
+        </div>
+        <div class="activity-container">
+            <div class="box" v-for="(item, index) in activityList" :key="index">
+                <img class="preview" src="../assets/img/activities-preview.jpg" />
+                <div class="content">
+                    {{item.title}} <br />
+                    {{item.type}}
+                    <router-link id="detail" to="/activityListDetail">详情</router-link>
+                    <!-- <button id="detail">详情</button> -->
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
+    export default {
 
-    data() {
-        return {
-            activityList: [
-                {title:'枪战训练1',type:'枪战训练'},
-                {title:"枪战对抗1",type:'枪战训练'},
-                {title:"枪战训练2",type:'枪战对抗'},
-                {title:"枪战对抗2",type:'枪战对抗'},
-                {title:'枪战训练1',type:'枪战训练'},
-                {title:"枪战对抗1",type:'枪战训练'},
-                {title:"枪战训练2",type:'枪战对抗'},
-                {title:"枪战对抗2",type:'枪战对抗'},
-                {title:'枪战训练1',type:'枪战训练'},
-                {title:"枪战对抗1",type:'枪战训练'},
+        data() {
+            return {
+                value5: 3.7,
+                hotActivityList: [
+                    { title: '枪战训练1', type: '枪战训练' },
+                    { title: "枪战对抗1", type: '枪战训练' },
+                    { title: "枪战训练2", type: '枪战对抗' },
+                    { title: "枪战对抗2", type: '枪战对抗' },
+                ],
+                activityList: [
+                    { title: '枪战训练1', type: '枪战训练' },
+                    { title: "枪战对抗1", type: '枪战训练' },
+                    { title: "枪战训练2", type: '枪战对抗' },
+                    { title: "枪战对抗2", type: '枪战对抗' },
+                    { title: '枪战训练1', type: '枪战训练' },
+                    { title: "枪战对抗1", type: '枪战训练' },
 
-            ]
+                ]
+            }
         }
     }
-}
 </script>
 
 <style scoped>
-.activity-container {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-family: 'Poppins',sans-serif;
-    flex-flow: wrap;
-}
-.box{
-    position: relative;
-    margin: 20px 30px;
-    width: 400px;
-    height: 300px;
-    display: flex;
-    flex-grow: 1;
-    justify-content: center;
-    flex-direction: row;
-    background: #060c2188;
-    box-shadow: 0 1px 3px 0 rgba(0,0,0,0.33);
-}
+    .activity-container {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: 'Poppins', sans-serif;
+        flex-flow: wrap;
+    }
 
-.preview {
-    width: 200px;
-    height: 200px;
-    flex-grow: 1;
-}
+    .box {
+        position: relative;
+        margin: 20px 30px;
+        width: 400px;
+        height: 300px;
+        display: flex;
+        flex-grow: 1;
+        justify-content: center;
+        flex-direction: row;
+        background: #060c2188;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.33);
+    }
+
+    .preview {
+        width: 200px;
+        height: 200px;
+        flex-grow: 1;
+    }
 
 
-.content
-{
-    flex-grow: 1;
-    padding: 20px;
-    box-sizing: border-box;
-    color: #fff;
-}
-#detail {
-    position: absolute;
-    right: 20px;
-    bottom: 20px;
-    color: #fff;
-    
-}
+    .content {
+        flex-grow: 1;
+        padding: 20px;
+        box-sizing: border-box;
+        color: #fff;
+    }
+
+    #detail {
+        position: absolute;
+        right: 20px;
+        bottom: 20px;
+        color: #fff;
+
+    }
+
+    .actContainer .hotActivity,
+    .actContainer .allActivity {
+        width: 600px;
+        height: 80px;
+        line-height: 80px;
+        margin: auto;
+        text-align: center;
+        border-bottom: 1px solid #fff;
+        color: #fff;
+        font-size: 26px;
+        margin-bottom: 20px;
+    }
 </style>
--- src/pages/activity/activityListDetail.vue
+++ src/pages/activity/activityListDetail.vue
@@ -10,7 +10,7 @@
             {{activityDetail.describe}}
         </div>
         <div class="join">
-            <button @click="join">加入活动</button>
+                <el-button type="primary" @click="join">加入活动</el-button>
         </div>
     </div>
 
@@ -22,11 +22,11 @@
         data(){
             return {
                 activityDetail:{
-                    title:'活动名称',
+                    title:'娱拓方案',
                     type:'枪战训练',
                     place:'场馆5',
                     time:'50分钟',
-                    describe:'活动描述',
+                    describe:'活动描述：中国企业的发展，经过了一段相当长时间的原始积累和个人英雄式的管理，已经进入到依靠团队和系统管理出效益的阶段。很多企业发展的瓶颈出现在人力资源发掘和管理上。如何在较短的时间内建设起一支强大的团队，是目前企业的共同诉求。上海西点卓越军团军事',
                     active:true//活动是否可参加
                 }
             }
@@ -43,8 +43,9 @@
     .activityDetail{
         width:90%;
         margin:50px auto;
-        background: #fff;
+        background: rgba(0,0,0,0.4);
         padding:50px;
+        color: #fff;
     }
     .activityDetail .title{
         text-align: center;
@@ -64,5 +65,6 @@
     }
     .activityDetail .join{
         text-align: center;
+        margin-top: 20px;;
     }
 </style>
