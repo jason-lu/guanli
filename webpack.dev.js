bc9e413abbcce851c47ac7173756d4dca2afeb89
Kuan Lu <czlukuan@gmail.com>
2019-11-16 14:59:28 +0800

    尝试修复mp4 loader bug

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,7 +26,6 @@ module.exports = merge(common, {
             {
                 test: /\.mp4$/,
                 use: 'file-loader',
-                loader: 'file-loader?name=videos/[name].[ext]',
             },
         ]
     },
