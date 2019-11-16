5f71c95a1ae6541b964c4a11c803d20c100a32ca
Kuan Lu <czlukuan@gmail.com>
2019-11-16 14:53:56 +0800

    修复bug

--- webpack.prod.js
+++ webpack.prod.js
@@ -32,7 +32,7 @@ module.exports = merge(common,{
                 use: 'file-loader',
                 loader: 'file-loader?name=videos/[name].[ext]',
             },
-    },
+        ],
     plugins:[
         new MiniCssExtractPlugin({
             filename:'style.css'
